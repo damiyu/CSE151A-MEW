b91606bc23bf7fabe1210d945f81750cdf2da0c1
damiyu <darrenyu2003@gmail.com>
2024-02-17 17:03:59 -0800

    data clean

--- src/main.py
+++ src/main.py
@@ -3,8 +3,7 @@ import matplotlib.pyplot as plt
 import sklearn as sk
 import json
 
+# Remove data with 4 or more missing features.
+
 file = open("./dataset/glassdoor_reviews.json", 'r', encoding='utf8')
 dataset = json.load(file)
-
-print("Dataset Size: %d" % (len(dataset)))
-print(f"Sample Item: {dataset[0]}")
--- src/models/baseline.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import sklearn as sk
-import json
-
-# Standard baseline model to set the bar for the other models.
-pass
