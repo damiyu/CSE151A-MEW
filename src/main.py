3f04884995817729aaac5044121b6ab9fe0af6f6
damiyu <darrenyu2003@gmail.com>
2024-03-14 16:49:38 -0700

    2/2 readme finished, viva las vegas!!!

--- src/main.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import sklearn as sk
-import json
-
-# Remove data with 4 or more missing features.
-
-file = open("./dataset/glassdoor_reviews.json", 'r', encoding='utf8')
-dataset = json.load(file)
